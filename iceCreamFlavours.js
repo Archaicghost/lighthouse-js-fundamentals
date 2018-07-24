4c7214e42f5898177bfb7f2b4885313d8bc12430
Zoe Gelech <archaic.ghost@gmail.com>
2018-07-24 18:08:44 +0000

    Adds conditional exercise

---
+++ iceCreamFlavours.js
@@ -0,0 +1,10 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+var rootbeer = "root beer";
+iceCreamFlavours.push(rootbeer);
+
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
